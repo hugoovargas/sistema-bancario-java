cdc5c200011c508777f30e8b43c2e3bb6f60057c
hugoovargas <hugoovvargas@gmail.com>
2026-06-25 10:41:57 -0300

    Introdução de Formatters e redução de acoplamento entre serviços

---
+++ src/main/java/ui/formatter/MoneyFormatter.java
@@ -0,0 +1,22 @@
+package ui.formatter;
+
+import model.valueobject.Money;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public final class MoneyFormatter {
+
+    private MoneyFormatter() {
+    }
+
+    public static String format(Money money) {
+        return NumberFormat
+                .getCurrencyInstance(
+                        Locale.of("pt", "BR")
+                ).format(
+                        money.value()
+                );
+    }
+}
